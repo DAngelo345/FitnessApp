76433a5897c4e820210b7f16b09fa227a88272fb
D'Angelo Thomas <dangelo1thomas@gmail.com>
2026-06-04 21:28:59 -0500

    users post route added and user get id route added

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,61 @@ app.get('/users', async (req, res) => {
   res.json(users);
 });
 
+app.get('/users/:id', async (req, res) => {
+  try {
+    const userId = Number(req.params.id);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        error: 'User not found',
+      });
+    }
+
+    res.json(user);
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: 'Something went wrong',
+    });
+  }
+});
+
+
+app.post('/users', async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { email, name, age, height, weight } = req.body;
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        age,
+        height,
+        weight,
+      },
+    });
+
+    res.status(201).json(user);
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: 'Something went wrong',
+    });
+  }
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
